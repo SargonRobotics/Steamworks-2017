7d26570afaafabd3b20aa1e6df40174049afeef6
Benjumanjibrah <hazlettbenjamin@gmail.com>
2017-01-11 17:50:35 -0600

    Added commands for drive train. Added drive train subsystem. Added crucial methods for deadzone and getting the axis.

--- src/org/usfirst/frc/team2335/robot/OI.java
+++ src/org/usfirst/frc/team2335/robot/OI.java
@@ -1,9 +1,32 @@
 package org.usfirst.frc.team2335.robot;
 
+import org.usfirst.frc.team2335.robot.commands.Drive;
+import org.usfirst.frc.team2335.robot.commands.Strafe;
+import org.usfirst.frc.team2335.robot.commands.Turn;
+import org.usfirst.frc.team2335.robot.triggers.Axis;
+
 import edu.wpi.first.wpilibj.Joystick;
 
 //This class connects operator control to different commands
 public class OI
 {	
 	Joystick controller = new Joystick(0);
+	
+	public OI()
+	{
+		//axes
+		Axis move = new Axis(controller, Robot.MOVE);
+		Axis rotate = new Axis(controller, Robot.ROTATE);
+		Axis strafe = new Axis(controller, Robot.STRAFE);
+		
+		//drive commands
+		move.whileActive(new Drive());
+		rotate.whileActive(new Turn());
+		strafe.whileActive(new Strafe());
+	}
+	
+	public double getAxis(int axis, double max)
+	{
+		return Robot.deadzone(controller.getRawAxis(axis), max);
+	}
 }
