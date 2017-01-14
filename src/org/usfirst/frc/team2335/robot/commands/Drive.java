aa09f270716529500396e74f16f5dfab19fb8393
BenjumanjiBrah <hazlettbenjamin@gmail.com>
2017-01-14 10:36:39 -0600

    Fixed spacing and removed comments

--- src/org/usfirst/frc/team2335/robot/commands/Drive.java
+++ src/org/usfirst/frc/team2335/robot/commands/Drive.java
@@ -4,34 +4,35 @@ import org.usfirst.frc.team2335.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-
-public class Drive extends Command {
-
-    public Drive() {
+public class Drive extends Command 
+{
+    public Drive() 
+    {
        requires(Robot.driveTrain);
     }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
+    
+    protected void initialize() 
+    {
+    
     }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    
+    protected void execute()
+    {
     	Robot.driveTrain.drive(Robot.oi.getAxis(Robot.MOVE, 0.95));
     }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    
+    protected boolean isFinished()
+    {
         return false;
     }
-
-    // Called once after isFinished returns true
-    protected void end() {
+    
+    protected void end() 
+    {
     	Robot.driveTrain.stopDrive();
     }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+    
+    protected void interrupted() 
+    {
+        end();
     }
 }
