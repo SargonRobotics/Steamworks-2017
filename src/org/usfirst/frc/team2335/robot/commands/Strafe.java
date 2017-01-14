9be0d18574c13f0d055503ab5df2a04ad758ccf5
BenjumanjiBrah <hazlettbenjamin@gmail.com>
2017-01-14 10:20:52 -0600

    Removed unnecessary comments and fixed spacing issues

--- src/org/usfirst/frc/team2335/robot/commands/Strafe.java
+++ src/org/usfirst/frc/team2335/robot/commands/Strafe.java
@@ -4,36 +4,35 @@ import org.usfirst.frc.team2335.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
-public class Strafe extends Command {
-
-    public Strafe() {
+public class Strafe extends Command 
+{
+    public Strafe() 
+    {
        requires(Robot.driveTrain);
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize()
+    {
+        
     }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
+    
+    protected void execute() 
+    {
     	Robot.driveTrain.strafe(Robot.oi.getAxis(Robot.STRAFE, 0.8));
     }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    
+    protected boolean isFinished() 
+    {
         return false;
     }
-
-    // Called once after isFinished returns true
-    protected void end() {
+    
+    protected void end() 
+    {
     	Robot.driveTrain.stopStrafe();
     }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+    
+    protected void interrupted() 
+    {
+        end();
     }
 }
