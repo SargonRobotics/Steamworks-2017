c09e2d8d936c84cfe5e0393f66adab84784c1bd5
Jack Moren <jack-m@live.com>
2017-01-14 10:02:33 -0600

    Fixed comments

--- src/org/usfirst/frc/team2335/robot/Robot.java
+++ src/org/usfirst/frc/team2335/robot/Robot.java
@@ -11,13 +11,6 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot
 {	
 	//Constants:
@@ -25,7 +18,7 @@ public class Robot extends IterativeRobot
 	
 	//Axes:
 	public static final int MOVE = 0, ROTATE = 0, SIDESTEP = 0;
-	//define constants to fit controllers
+	//Define constants for controllers
 	
 	//Motor ports:
 	public static final int LEFTDRIVE = 0, RIGHTDRIVE = 1, STRAFE = 2;
